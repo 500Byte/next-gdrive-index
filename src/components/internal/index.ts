75fcba0902c37c44f008262c32a85e774a21a9a0
Diego N. <diegoleonsmr0@gmail.com>
2026-04-27 14:10:30 -0500

    chore: clean up internal components exports

--- src/components/internal/index.ts
+++ src/components/internal/index.ts
@@ -1,7 +1,1 @@
-export { default as ConfiguratorPage } from "./ConfiguratorPage";
-export { default as DeployPage } from "./DeployPage";
 export { default as EmbedPage } from "./EmbedPage";
-
-export { default as APIConfigurator } from "./ConfiguratorPage.Api";
-export { default as EnvironmentConfigurator } from "./ConfiguratorPage.Environment";
-export { default as SiteConfigurator } from "./ConfiguratorPage.Site";
