5ed49c3dd78deb3adb826998b2ea29a307d061eb
Diego N. <diegoleonsmr0@gmail.com>
2026-04-27 14:10:35 -0500

    chore: remove deprecated configuration schemas
    Remove unused schemas:
    - Schema_v1_Config
    - Schema_v2_3_Config
    - Schema_v2_3_Config_Site
    - Schema_App_Configuration
    - Schema_App_Configuration_Env
    - Related type exports

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -40,56 +40,6 @@ export const Schema_File = z.object({
     .nullable(),
 });
 
-export const Schema_v1_Config = z.object({
-  version: z.literal("1.0.0"),
-  basePath: z.string(),
-  masterKey: z.string(),
-  cacheControl: z.string(),
-
-  apiConfig: z.object({
-    rootFolder: z.string(),
-    isTeamDrive: z.boolean(),
-    sharedDrive: z.string().optional(),
-    defaultQuery: z.array(z.string()),
-    defaultField: z.string(),
-    defaultOrder: z.string(),
-    itemsPerPage: z.number().positive(),
-    searchResult: z.number().positive(),
-
-    specialFile: z.object({
-      password: z.string(),
-      readme: z.string(),
-      banner: z.string(),
-    }),
-    hiddenFiles: z.array(z.string()),
-
-    allowDownloadProtectedFile: z.boolean(),
-    temporaryTokenDuration: z.number().positive(),
-    maxFileSize: z.number().positive(),
-  }),
-
-  siteConfig: z.object({
-    siteName: z.string(),
-    siteDescription: z.string(),
-    siteIcon: z.string(),
-    favIcon: z.string(),
-    twitterHandle: z.string().optional().default("@__mbaharip__"),
-
-    defaultAccentColor: z.string(),
-
-    privateIndex: z.boolean().optional().default(false),
-
-    navbarItems: z.array(
-      z.object({
-        icon: z.string(),
-        name: z.string(),
-        href: z.string(),
-        external: z.boolean().optional().default(false),
-      }),
-    ),
-  }),
-});
-
 const Schema_Config_API = z
   .object({
     rootFolder: z.string(),
@@ -122,50 +72,6 @@ const Schema_Config_API = z
     { message: "sharedDrive is required when isTeamDrive is true" },
   );
 
-const Schema_v2_3_Config_Site = z.object({
-  siteName: z.string(),
-  siteNameTemplate: z.string().optional().default("%s"),
-  siteDescription: z.string(),
-  siteIcon: z.string(),
-  siteAuthor: z.string().optional().default("mbaharip"),
-  favIcon: z.string(),
-  robots: z.string().optional().default("noindex, nofollow"),
-  twitterHandle: z.string().optional().default("@__mbaharip__"),
-
-  showFileExtension: z.boolean().optional().default(false),
-
-  footer: z.string().array().optional(),
-
-  privateIndex: z.boolean().optional().default(false),
-  breadcrumbMax: z.number(),
-
-  toaster: z
-    .object({
-      position: z.enum(["top-left", "top-right", "bottom-left", "bottom-right"]),
-      duration: z.number().positive(),
-    })
-    .optional()
-    .default({
-      position: "top-right",
-      duration: 5000,
-    }),
-
-  navbarItems: z.array(
-    z.object({
-      icon: z.enum(Object.keys(icons) as [keyof typeof icons]),
-      name: z.string(),
-      href: z.string(),
-      external: z.boolean().optional().default(false),
-    }),
-  ),
-  supports: z.array(
-    z.object({
-      name: z.string(),
-      currency: z.string(),
-      href: z.string(),
-    }),
-  ),
-});
 export const Schema_Config_Site = z.object({
   siteName: z.string(),
   siteNameTemplate: z.string().optional().default("%s"),
@@ -228,32 +134,14 @@ export const Schema_Config_Site = z.object({
   }),
 });
 
-export const Schema_v2_3_Config = z.object({
-  version: z.string(),
-  basePath: z.string(),
-  cacheControl: z.string(),
-  showDeployGuide: z.boolean(),
-
-  apiConfig: Schema_Config_API,
-
-  siteConfig: Schema_v2_3_Config_Site,
-});
 export const Schema_Config = z.object({
   version: z.string(),
   basePath: z.string(),
   cacheControl: z.string(),
-  showGuideButton: z.boolean(),
 
   apiConfig: Schema_Config_API,
 
   siteConfig: Schema_Config_Site,
-});
-
-export const Schema_App_Configuration_Env = z.object({
-  GD_SERVICE_B64: z.string(),
-  ENCRYPTION_KEY: z.string(),
-  SITE_PASSWORD: z.string().optional(),
-  NEXT_PUBLIC_DOMAIN: z.string().optional(),
 });
 
 export const Schema_ServiceAccount = z.object({
@@ -269,35 +157,6 @@ export const Schema_ServiceAccount = z.object({
   client_x509_cert_url: z.string().url(),
   universe_domain: z.string().optional(),
 });
-
-export const Schema_App_Configuration = z.object({
-  version: z.string(),
-  environment: Schema_App_Configuration_Env,
-  api: Schema_Config_API.and(
-    z.object({
-      cache: z.object({
-        public: z.coerce.boolean(),
-        maxAge: z.coerce.number().min(0),
-        sMaxAge: z.coerce.number().min(0),
-        staleWhileRevalidate: z.coerce.boolean(),
-      }),
-    }),
-  ),
-  site: Schema_Config_Site.and(
-    z.object({
-      guideButton: z.boolean(),
-    }),
-  ),
-});
-
-export type ConfigurationCategory = keyof z.infer<typeof Schema_App_Configuration>;
-export type ConfigurationKeys<T extends keyof z.infer<typeof Schema_App_Configuration>> = keyof z.infer<
-  typeof Schema_App_Configuration
->[T];
-export type ConfigurationValue<
-  T extends keyof z.infer<typeof Schema_App_Configuration>,
-  K extends keyof z.infer<typeof Schema_App_Configuration>[T],
-> = z.infer<typeof Schema_App_Configuration>[T][K];
 
 export const Schema_FileToken = z.object({
   id: z.string(),
