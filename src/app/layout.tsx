c125a24f2ebc3053b51d688f8179179b14799420
Diego N. <diegoleonsmr0@gmail.com>
2026-04-29 18:54:24 -0500

    feat: integrate geist fonts (sans + mono)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,18 @@
 import { type Metadata } from "next";
-import { Outfit } from "next/font/google";
 import { headers } from "next/headers";
 
 import { DashboardLayout } from "~/components/layout";
 import { Provider } from "~/components/layout";
 import { Password } from "~/components/layout";
 
-import { cn } from "~/lib/utils";
 import { CheckIndexPassword } from "~/actions/password";
 import { type ActionResponseSchema } from "~/types";
 
 import "~/styles/globals.css";
 import config from "config";
 
-const outfit = Outfit({
-  weight: ["300", "400", "600", "700"],
-  subsets: ["latin", "latin-ext"],
-  variable: "--font-outfit",
-});
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -30,7 +25,6 @@ export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const head = await headers();
-  const pathname = head.get("X-Pathname") ?? "/";
 
   let unlocked: ActionResponseSchema = {
     success: true,
@@ -43,7 +37,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("font-sans", outfit.variable)}>
+      <body className={`${GeistSans.variable} ${GeistMono.variable} font-sans antialiased`}>
         <Provider>
           {config.siteConfig.privateIndex && !unlocked.success ? (
             <Password type="global" errorMessage={unlocked.error} />
